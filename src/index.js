f248d3d4371294566173438eb9ae36fa9e9f9347
barathraj048 <129966260+barathraj048@users.noreply.github.com>
2023-10-23 17:39:41 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import reactDOM from "react-dom";
+reactDOM.render(
+  <div>
+    <ul>
+      <h1>Fav food to king</h1>
+      <li>briyani</li>
+      <li>full ind meals</li>
+      <li>non veg meals</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
